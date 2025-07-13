ec47257aab1fb6b1f607cae4089c613f06d4cfc8
Aleix Tulko <alexeitulko@gmail.com>
2025-07-13 13:49:28 +0200

    req

--- kafka_streams.py
+++ kafka_streams.py
@@ -1,4 +1,9 @@
+from threading import Thread
+
 import faust
+from confluent_kafka import (
+    Consumer, KafkaError, KafkaException, Producer
+)
 
 
 prohibited_users = {
@@ -44,6 +49,46 @@ filtered_messages_topic = app.topic(
 blocked_users_topic = app.topic(
     'blocked_users', key_type=str, value_type=BlockedUsers
 )
+
+
+def create_message(sender_id: int, sender_name: str,
+                   recipient_id: int, recipient_name: str,
+                   amount: float, content: str
+) -> None:
+    """Сериализация сообщения и отправка в брокер."""
+    message_value = {
+        "sender_id": sender_id,
+        "sender_name": sender_name,
+        "recipient_id": recipient_id,
+        "recipient_name": recipient_name,
+        "amount": amount,
+        "content": content
+        }
+    producer.produce(
+        topic=TOPIC,
+        key=key_serializer(
+            "user_key", SerializationContext(TOPIC, MessageField.VALUE)
+        ),
+        value=value_serializer(
+            message_value, SerializationContext(TOPIC, MessageField.VALUE)
+        ),
+        on_delivery=delivery_report
+    )
+
+
+def producer_infinite_loop():
+    """Запуска цикла для генерации сообщения."""
+    incr_num = 0
+    try:
+        while True:
+            create_message(incr_num)
+            incr_num += 1
+            if incr_num % 10 == 0:
+                producer.flush()
+    except Exception as e:
+        raise RuntimeError(e)
+    finally:
+        producer.flush()
 
 
 @app.agent(messages_topic)
