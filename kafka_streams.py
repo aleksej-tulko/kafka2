c681264c9c67446dd07783405dbf51b8a490d890
Aleix Tulko <alexeitulko@gmail.com>
2025-07-13 12:52:27 +0200

    msg

--- kafka_streams.py
+++ kafka_streams.py
@@ -1,9 +1,14 @@
 import faust
 
 
+PROHIBITED_USERS = ['spammer']
+
+
 class Transaction(faust.Record):
-    sender_id: str
-    recipient_id: str
+    sender_id: int
+    sender_name: str
+    recipient_id: int
+    recipient_name: str
     amount: float
     content: str
 
@@ -17,21 +22,25 @@ app = faust.App(
 table = app.Table(
     "blocked_users",
     partitions=3,
-    default=lambda x: x == "spammer"
+    default=str
 )
 
-transactions_topic = app.topic(
-    "transactions", key_type=str, value_type=Transaction
+messages_topic = app.topic(
+    'messages', key_type=str, value_type=Transaction
 )
-fraud_detection_topic = app.topic(
-    "fraud-detection", key_type=str, value_type=Transaction
+filtered_messages_topic = app.topic(
+    'filtered_messages', key_type=str, value_type=Transaction
+)
+
+blocked_users_topic = app.topic(
+    'blocked_users', key_type=str, value_type=Transaction
 )
 
 
-@app.agent(transactions_topic)
-async def process_transactions(stream):
-    async for transaction in stream:
-        if transaction.amount > 10_000:
-            await fraud_detection_topic.send(
-                value=transaction
+@app.agent(messages_topic)
+async def process_messages(stream):
+    async for message in stream:
+        if message.amount > 10_000:
+            await filtered_messages_topic.send(
+                value=message
             )
