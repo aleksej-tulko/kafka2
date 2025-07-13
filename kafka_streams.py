f0c9e7422d4a61441b3b34b64c35fe7786ca35c6
Aleix Tulko <alexeitulko@gmail.com>
2025-07-13 20:36:58 +0200

    зкште

--- kafka_streams.py
+++ kafka_streams.py
@@ -1,19 +1,4 @@
-from threading import Thread
-from time import sleep
-
 import faust
-
-prohibited_users = {
-    "clown": ["spammer"],
-    "spammer": ["dodik"],
-    "dodik": ["clown"]
-}
-
-
-class BlockedUsers(faust.Record):
-    blocker: str
-    blocked: list[str]
-
 
 class Messages(faust.Record):
     sender_id: int
@@ -23,45 +8,19 @@ class Messages(faust.Record):
     amount: float
     content: str
 
-
 app = faust.App(
     "pract-task3",
     broker="kafka://localhost:9093,localhost:9095,localhost:9097",
-    store="rocksdb://",
-)
-
-table = app.Table(
-    "blocked_users",
-    partitions=3,
-    default=str
 )
 
 messages_topic = app.topic(
     'messages', key_type=str, value_type=Messages
 )
-filtered_messages_topic = app.topic(
-    'filtered_messages', key_type=str, value_type=Messages
-)
-
-blocked_users_topic = app.topic(
-    'blocked_users', key_type=str, value_type=BlockedUsers
-)
-
 
 @app.agent(messages_topic)
 async def filter_blocked_users(stream):
     async for message in stream:
-        sender = message.sender_name
-        recipient = message.recipient_name
+        print(f"Sender: {message.sender_name}, Recipient: {message.recipient_name}")
 
-        print(recipient)
-        print(sender)
-
-        # if sender in prohibited_users.get(recipient, []):
-        #     await filtered_messages_topic.send(
-        #         key=recipient,
-        #         value=BlockedUsers(
-        #             blocker=recipient,
-        #             blocked=[sender]
-        #         )
-        #     )
+if __name__ == '__main__':
+    app.main()
