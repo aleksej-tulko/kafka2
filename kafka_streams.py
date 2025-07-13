747541fa203e852d740c63d55e106bcffc24d960
Aleix Tulko <alexeitulko@gmail.com>
2025-07-13 14:50:07 +0200

    prod

--- kafka_streams.py
+++ kafka_streams.py
@@ -77,6 +77,12 @@ producer_conf = conf | {
 
 producer = Producer(producer_conf)
 
+user_ids = {
+    "clown": 1,
+    "spammer": 2,
+    "dodik": 3
+}
+
 prohibited_users = {
     "clown": ["spammer"],
     "spammer": ["dodik"],
@@ -157,13 +163,23 @@ def create_message(sender_id: int, sender_name: str,
 
 def producer_infinite_loop():
     """Запуска цикла для генерации сообщения."""
-    incr_num = 0
+    incr_num: float = 0.0
+    recipient_names = [name for name in user_ids]
     try:
         while True:
-            create_message(incr_num)
-            incr_num += 1
-            if incr_num % 10 == 0:
-                producer.flush()
+            for sender_name, id in user_ids.items():
+                for recipient_name in recipient_names:
+                    if sender_name != recipient_name:
+                        create_message(
+                            sender_id=id,
+                            sender_name=sender_name,
+                            recipient_id=user_ids[recipient_name],
+                            recipient_name=recipient_name,
+                            amount=incr_num,
+                            content=f'{incr_num}')
+                        incr_num += 1.0
+                        if incr_num % 10 == 0:
+                            producer.flush()
     except Exception as e:
         raise RuntimeError(e)
     finally:
