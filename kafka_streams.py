957ac1b5f4ad74e74209f43d47805cbbef6c0bab
Aleix Tulko <alexeitulko@gmail.com>
2025-07-17 15:53:29 +0200

    msg

--- kafka_streams.py
+++ kafka_streams.py
@@ -89,5 +89,6 @@ async def save_blocked_to_db(stream):
     async for message in stream.take(
         len(all_blocked), 10.0
     ):
-        table[message.blocker] = message.blocked
-        print(table[message.blocker])
+        print(message)
+        # table[message.blocker] = message.blocked
+        # print(table[message.blocker])
