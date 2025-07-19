1406c51af4669309b3ab3dc87a6777202f94f84d
Aleix Tulko <alexeitulko@gmail.com>
2025-07-19 13:18:45 +0200

    her

--- kafka_streams.py
+++ kafka_streams.py
@@ -106,6 +106,9 @@ async def filter_messages():
         processors=[lower_str_input, mask_bad_words]
     )
     async for message in processed_stream:
-        if table:
-            if message.sender_name not in table[message.recipient_name]:
+        if not table:
+            await filter_messages.send(value=message)
+        else:
+            recipient = [message.recipient_name]
+            if table[recipient] and message.sender_name not in table[recipient]:
                 await filter_messages.send(value=message)
