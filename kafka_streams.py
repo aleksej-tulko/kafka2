6f5cd666ad42d6f743631d86436b147d078526b3
Aleix Tulko <alexeitulko@gmail.com>
2025-07-19 14:35:46 +0200

    svsr

--- kafka_streams.py
+++ kafka_streams.py
@@ -102,5 +102,4 @@ async def filter_messages():
     async for message in processed_stream:
         if message.sender_name in table[message.recipient_name]:
             print(f'{message.recipient_name} заблокировал {message.sender_name}.')
-            continue
         await filtered_messages_topic.send(value=message)
