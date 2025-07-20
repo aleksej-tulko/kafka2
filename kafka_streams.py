f91e6a37fb463f1055ea454f3abbac09612f1117
Aleix Tulko <alexeitulko@gmail.com>
2025-07-20 13:30:29 +0200

    цуцм

--- kafka_streams.py
+++ kafka_streams.py
@@ -126,8 +126,8 @@ async def filter_blocked_users(stream):
 
 @app.agent(messages_topic)
 async def count_frequency(stream):
+    cycle_start_time = datetime.now()
     async for message in stream:
-        cycle_start_time = datetime.now()
         messages_frequency_table[message.sender_name] += 1
         value = messages_frequency_table[message.sender_name]
         now_value = value.now() or 0
