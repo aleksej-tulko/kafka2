d49cf37dd4d0c06fb60deb6a92c3e83307724959
Aleix Tulko <alexeitulko@gmail.com>
2025-07-20 16:29:41 +0200

    vsb

--- kafka_streams.py
+++ kafka_streams.py
@@ -166,8 +166,10 @@ async def count_frequency(stream):
 @app.task
 async def filter_messages():
     processed_stream = app.stream(
-        messages_topic
+        messages_topic,
+        processors=[lower_str_input, mask_bad_words]
     )
+    print(processed_stream)
     async for message in processed_stream:
-        print('ev')
-        await filtered_messages_topic.send(value=message)
+        if message.sender_name not in table[message.recipient_name]:
+            await filtered_messages_topic.send(value=message)
