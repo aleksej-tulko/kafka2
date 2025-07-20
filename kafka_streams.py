220bf7fc457fed96faf4e0724e3017fb14742a0e
Aleix Tulko <alexeitulko@gmail.com>
2025-07-20 16:21:45 +0200

    sdv

--- kafka_streams.py
+++ kafka_streams.py
@@ -169,8 +169,7 @@ async def filter_messages():
         messages_topic,
         processors=[lower_str_input, mask_bad_words]
     )
+    print('heh')
     async for message in processed_stream:
-        print('her')
         if message.sender_name not in table[message.recipient_name]:
-            print('kek')
             await filtered_messages_topic.send(value=message)
