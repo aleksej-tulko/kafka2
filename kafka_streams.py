45aa71897389bdbdabe9f18e4d94666cde2b183b
Aleix Tulko <alexeitulko@gmail.com>
2025-07-20 22:52:31 +0200

    cont

--- kafka_streams.py
+++ kafka_streams.py
@@ -176,5 +176,6 @@ async def filter_messages(stream): # Отправка в отстортиров�
         processors=[lower_str_input, mask_bad_words] # Обработка
     )
     async for message in processed_stream:
-        if message.sender_name not in table[message.recipient_name]: # Проверка цензуры
-            await filtered_messages_topic.send(value=message)
+        if message.sender_name in table[message.recipient_name]: # Проверка цензуры
+            continue
+        await filtered_messages_topic.send(value=message)
