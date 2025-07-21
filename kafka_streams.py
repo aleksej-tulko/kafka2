84783b70adfd3e2da2a66692d6606d0a3a31379c
Aleix Tulko <alexeitulko@gmail.com>
2025-07-21 20:33:15 +0200

    edcsd

--- kafka_streams.py
+++ kafka_streams.py
@@ -76,12 +76,12 @@ app.conf.consumer_auto_offset_reset = "earliest"
 
 blocked_senders_table = app.Table( # Таблица, где постоянно хранятся списки заблокированных.
     "blocked-users-table",
-    partitions=2,
+    partitions=1,
     default=list,
     changelog_topic=app.topic( # При рестарте или сбое данные будут восстановлены из этого топика.
         "blocked-users-changelog",
         value_type=BlockedUsers(blocker=str, blocked=list[str]),
-        partitions=2
+        partitions=1
     )
 )
 
@@ -98,7 +98,7 @@ bad_words_table = app.Table( # Таблица, где постоянно хра�
 
 messages_frequency_table = app.Table( # Таблица для отслеживания кол-во сообщений за время жизни окна.
     "messages_frequency",
-    partitions=2,
+    partitions=1,
     default=int
 ).hopping(
     WINDOW_RANGE,
